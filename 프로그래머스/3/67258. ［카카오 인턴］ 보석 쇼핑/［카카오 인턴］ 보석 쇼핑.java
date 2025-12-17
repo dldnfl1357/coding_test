49a1857fdc540b2b702ccadd32a23813c9f6d2fc
dldnfl1357 <54130917+dldnfl1357@users.noreply.github.com>
2025-12-18 00:59:51 +0900

    Optimize gem tracking in solution method
    Refactor gem tracking from HashSet to HashMap for efficiency.

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,28 +1,34 @@
 import java.util.*;
 class Solution {
+    // 최악의 경우 O(400,000)
     public int[] solution(String[] gems) {
         int[] answer = {};
 
         HashSet<String> gemSet = new HashSet<>();
-        HashSet<String> nowGemSet = new HashSet<>();
+        HashMap<String, Integer> nowGemSet = new HashMap<>();
 
         //O(100,000)
         for(String gem : gems)
             gemSet.add(gem);
 
         int start = 0;
-        int end = 0;
+        int end = -1;
         int min = Integer.MAX_VALUE;
-        //O(100,000)
+
+        //O(100,000 * 3)
         while(end < gems.length) {
-            nowGemSet.add(gems[end]);
-            if(gemSet.equals(nowGemSet)) {
-                if(min > end - start + 1)
+            if(gemSet.size() == nowGemSet.size()) { // O(1)
+                if(min > end - start + 1) {
                     min = end - start + 1;
+                    answer = new int[]{start+1, end+1};
+                }
+                nowGemSet.merge(gems[start], -1, (old, d) -> old + d <= 0 ? null : old + d); // O(1)
                 start++;
             }
             else {
                 end++;
+                if(end >= gems.length) break;
+                nowGemSet.merge(gems[end], 1, Integer::sum); // O(1)
             }
         }
         return answer;
