beb8526cad5e6fa5c848e5381aaf6c8d90c19e04
dldnfl1357 <54130917+dldnfl1357@users.noreply.github.com>
2025-12-18 00:55:33 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 49.71 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -4,30 +4,25 @@ class Solution {
         int[] answer = {};
 
         HashSet<String> gemSet = new HashSet<>();
-        HashMap<String, Integer> nowGemSet = new HashMap<>();
+        HashSet<String> nowGemSet = new HashSet<>();
 
         //O(100,000)
         for(String gem : gems)
             gemSet.add(gem);
 
         int start = 0;
-        int end = -1;
+        int end = 0;
         int min = Integer.MAX_VALUE;
-
         //O(100,000)
         while(end < gems.length) {
-            if(gemSet.size() == nowGemSet.size()) {
-                if(min > end - start + 1) {
+            nowGemSet.add(gems[end]);
+            if(gemSet.equals(nowGemSet)) {
+                if(min > end - start + 1)
                     min = end - start + 1;
-                    answer = new int[]{start+1, end+1};
-                }
-                nowGemSet.merge(gems[start], -1, (old, d) -> old + d <= 0 ? null : old + d);
                 start++;
             }
             else {
                 end++;
-                if(end >= gems.length) break;
-                nowGemSet.merge(gems[end], 1, Integer::sum);
             }
         }
         return answer;
